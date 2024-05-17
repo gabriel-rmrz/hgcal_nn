a30e3f745f91fb137ea30fac15dd9e146fa50afb
gabriel-rmrz <gabriel-rmrz@users.noreply.github.com>
2024-05-17 14:31:08 +0200

    cleaning prepare_inputs script, working now

--- prepare_inputs.py
+++ prepare_inputs.py
@@ -119,18 +119,13 @@ def prepare_fromSim(filename, suf, bins= [6,6,6], isPU=False):
 
   # Creating the voxels for the different trackstersMerged
   #features_perTracksterMerged
-  fTsM_en = ak.ArrayBuilder()
   fTsM_en_cls= ak.ArrayBuilder()
-  fTsM_g3D = ak.ArrayBuilder()
   fTsM_tiles = ak.ArrayBuilder()
   fTsM_pos = ak.ArrayBuilder()
   fTsM_scalars = ak.ArrayBuilder()
   fTsM_eta_phi = ak.ArrayBuilder()
-  fTsM_1D = ak.ArrayBuilder()
   fTsM_1D_cls = ak.ArrayBuilder()
-  deltaTsM_1D = ak.ArrayBuilder()
   deltaTsM_1D_cls = ak.ArrayBuilder()
-  deltaTsM_eta_phi_z_1D = ak.ArrayBuilder()
   deltaTsM_eta_phi_z_1D_cls = ak.ArrayBuilder()
   #target_perTracksterMerged
   fTsM_pos_cls = ak.ArrayBuilder()
@@ -141,79 +136,71 @@ def prepare_fromSim(filename, suf, bins= [6,6,6], isPU=False):
   ev_info =  ak.ArrayBuilder()
 
 
-  for i, ev in enumerate(simToReco_index): # looping over all the events
-  #for i, ev in enumerate(simToReco_index[:601]): # looping over all the events
+  none_reco = 0
+  for i, s2r_ind in enumerate(simToReco_index): # looping over all the events
+  #for i, s2r_ind in enumerate(simToReco_index[:101]): # looping over all the events
     if not (i%100) :
       print(f"%%%%%%%%%%%%%%% Event {i} %%%%%%%%%%%%%%%")
-    isPassScore0 =simToReco_score[i] < 0.35
-    if np.sum(isPassScore0, axis=None) < 1:
-      continue
-    en_ratio = simToReco_en[i][isPassScore0]/simTICLCandidate_raw_energy[i] 
-    #print(f"en_ratio: {en_ratio}")
-    en_ratio = flatten_array(en_ratio)
-    max_i = np.argmax(en_ratio)
-    s = ev[isPassScore0]  # Tracksters in the in the tracksterMergeds that passed the score cut..
-    ## TODO: Use all the s' , then flatten, and compute unique.
-    #print(f"s: {s}")
-    if len(s[max_i]) > 1:
-      print(f"s[max_i]: {s[max_i]}")
-    #print(f"max_i: {max_i}")
-    s = s[max_i] # tracksterMerged with highest energy ratio.
-    j = max_i
-    #print(f"tsLinkedInCand[i][s]: {tsLinkedInCand[i][s]}")
-    #print(f"bxTsM[i]: {bxTsM[i]}")
-    #print(f"bxTsM[i][s]: {bxTsM[i][s]}")
-    mTs = tsLinkedInCand[i][s]
-    mTs_en = simToReco_en[i][j]
-    mTs = ak.flatten(tsLinkedInCand[i][s])
-    #print(f"mTs: {mTs}")
-    if len(mTs)==0:
-      continue
-    ev_info.append(np.array([i,max_i]))
+    
+    for j in range(len(s2r_ind)):
+      isPassScore0 = simToReco_score[i][j] < 0.35
+      if np.sum(isPassScore0, axis=None) < 1:
+        none_reco+=1
+        continue
+      ind = np.arange(len(isPassScore0),dtype=int)
+      ind = ind[isPassScore0]
+      s2r_ind_j = s2r_ind[j][isPassScore0]  # tracksterMerged that passed the score cut..
+      for k, s in zip(ind,s2r_ind_j):
 
-    ###################################
-    ## Defition of the inputs
-    #glob_pos = np.array([bxTs[i][mTs], byTs[i][mTs], bzTs[i][mTs]])
-    glob_pos = np.array([bxTsM[i][s], byTsM[i][s], bzTsM[i][s]])
-    #print(glob_pos)
-    #glob_eta_phi = np.array([betaTs[i][mTs], bphiTs[i][mTs]])
-    glob_eta_phi = np.array([betaTsM[i][s], bphiTsM[i][s]])
 
-    vtxIds = ak.flatten(vtxIdTs[i][mTs])
-    betaClsInTs = betaCls[i][vtxIds]
-    bphiClsInTs = bphiCls[i][vtxIds]
-    bxClsInTs = bxCls[i][vtxIds]
-    byClsInTs = byCls[i][vtxIds]
-    bzClsInTs = bzCls[i][vtxIds]
-    glob_pos_cls = np.array([bxClsInTs, byClsInTs, bzClsInTs])
-    glob_eta_phi_cls = np.array([betaClsInTs, bphiClsInTs])
+        ## TODO: Use all the s' , then flatten, and compute unique.
+        mTs = tsLinkedInCand[i][s]
+        mTs_en = simToReco_en[i][j][k]
+        mTs = tsLinkedInCand[i][s]
+        if len(mTs)==0:
+          continue
+        ev_info.append(np.array([i,j]))
 
-    fTsM_pos_cls.append(glob_pos_cls)
-    fTsM_eta_phi_cls.append(glob_eta_phi_cls)
-    fTsM_pos.append(glob_pos)
-    fTsM_scalars.append(np.array([betaClsInTs, bxClsInTs]))
-    fTsM_eta_phi.append(glob_eta_phi)
-    fTsM_en.append(raw_enTs[i][mTs])
-    fTsM_en_cls.append(enCls[i][vtxIds])
+        ###################################
+        ## Defition of the inputs
+        #glob_pos = np.array([bxTs[i][mTs], byTs[i][mTs], bzTs[i][mTs]])
+        glob_pos = np.array([bxTsM[i][s], byTsM[i][s], bzTsM[i][s]])
+        #glob_eta_phi = np.array([betaTs[i][mTs], bphiTs[i][mTs]])
+        glob_eta_phi = np.array([betaTsM[i][s], bphiTsM[i][s]])
 
-    ###################################
-    ## Defition of the target
-    ##################################
-    max_score_r2s = 0.35
-    min_energy_r2s = 0.5
-    #isPass = ((recoToSim_score[i][j] < max_score_r2s) & (recoToSim_en[i][j] > min_energy_r2s))
-    isPassScore = recoToSim_score[i][j] < max_score_r2s 
-    #en_frac = recoToSim_en[i][j][isPassScore]/simTICLCandidate_regressed_energy[i][recoToSim_index[i][j][isPassScore]]
-    #en_frac = simToReco_en[i][j][isPassScore]/simtrackstersCP_raw_energy[i][j]
-    en_frac = mTs_en/simtrackstersCP_raw_energy[i][j]
+        vtxIds = ak.flatten(vtxIdTs[i][mTs])
+        betaClsInTs = betaCls[i][vtxIds]
+        bphiClsInTs = bphiCls[i][vtxIds]
+        bxClsInTs = bxCls[i][vtxIds]
+        byClsInTs = byCls[i][vtxIds]
+        bzClsInTs = bzCls[i][vtxIds]
+        glob_pos_cls = np.array([bxClsInTs, byClsInTs, bzClsInTs])
+        glob_eta_phi_cls = np.array([betaClsInTs, bphiClsInTs])
 
-    isPass = False
-    if np.any(en_frac > 0.5):
-      isPass = True
-    tTsM.append(isPass)
+        fTsM_pos_cls.append(glob_pos_cls)
+        fTsM_eta_phi_cls.append(glob_eta_phi_cls)
+        fTsM_pos.append(glob_pos)
+        fTsM_scalars.append(np.array([betaClsInTs, bxClsInTs]))
+        fTsM_eta_phi.append(glob_eta_phi)
+        fTsM_en_cls.append(enCls[i][vtxIds])
+
+        ###################################
+        ## Defition of the target
+        ##################################
+        max_score_s2r = 0.35
+        min_energy_s2r = 0.5
+        #isPass = ((recoToSim_score[i][j][k] < max_score_s2r) & (recoToSim_en[i][j][k] > min_energy_s2r))
+        isPassScore = simToReco_score[i][j][k] < max_score_s2r 
+        #en_frac = recoToSim_en[i][j][k][isPassScore]/simTICLCandidate_regressed_energy[i][recoToSim_index[i][j][k][isPassScore]]
+        #en_frac = simToReco_en[i][j][k][isPassScore]/simtrackstersCP_raw_energy[i][j]
+        en_frac = mTs_en/simtrackstersCP_raw_energy[i][j]
+
+        isPass = False
+        if np.any(en_frac > 0.5):
+          isPass = True
+        tTsM.append(isPass)
 
   ev_info = ev_info.snapshot()
-  fTsM_en = save_array_to_file(fTsM_en, f"{suf}_en.parquet")
   fTsM_en_cls = save_array_to_file(fTsM_en_cls, f"{suf}_en_cls.parquet")
   fTsM_pos = save_array_to_file(fTsM_pos, f"{suf}_pos.parquet")
   fTsM_eta_phi = save_array_to_file(fTsM_eta_phi, f"{suf}_eta_phi.parquet")
@@ -226,29 +213,15 @@ def prepare_fromSim(filename, suf, bins= [6,6,6], isPU=False):
 
   z_layers = np.array([322, 323, 325, 326, 328, 329, 331, 332, 334, 335, 337, 338, 340,341, 343, 344, 346, 347, 349, 350, 353, 354, 356, 357, 360, 361,
                   367, 374, 380, 386, 393, 399, 405, 411, 412, 418, 424, 430, 431, 439, 447, 455, 463, 471, 472, 480, 488, 496, 504, 505, 513], dtype=np.int32)
+  print(f"fTsM_pos.type: {fTsM_pos.type}")
   for i in range(len(fTsM_pos)):
     if not (i%100) :
       print(f"%%%%%%%%%%%%%%% Candidate {i} %%%%%%%%%%%%%%%")
-    pos_ts = fTsM_pos[i,:,:]
-    eta_phi_ts = fTsM_eta_phi[i,:,:]
-    pos_cls = np.asarray(fTsM_pos_cls[i,:,:])
+    pos_cls = np.asarray(fTsM_pos_cls[i])
     pos_cls[2,:] = np.absolute(pos_cls[2,:])
-    eta_phi_cls = fTsM_eta_phi_cls[i,:,:]
-    en_ts = fTsM_en[i]
+    eta_phi_cls = fTsM_eta_phi_cls[i]
     en_cls = fTsM_en_cls[i]
-    betaTs_mean = np.mean(eta_phi_ts[0,:])
-    delta_etaTs = eta_phi_ts[0,:] - betaTs_mean
-    bxTs_mean = np.mean(pos_ts[0,:])
-    delta_xTs = pos_ts[0,:] - bxTs_mean
-    byTs_mean = np.mean(pos_ts[1,:])
-    delta_yTs = pos_ts[1,:] - byTs_mean
-    bzTs_mean = np.mean(pos_ts[2,:])
-    delta_zTs = pos_ts[2,:] - bzTs_mean
-    delta_phiTs, bphiTs_mean = get_delta_phi(eta_phi_ts[1,:])
-    #TODO: Check the 1.1 factor for the mins.
 
-    rel_pos = np.array([delta_xTs, delta_yTs, delta_zTs]).T
-    rel_eta_phi_z = np.array([delta_etaTs, delta_phiTs, delta_zTs]).T
 
     betaCls_mean = np.mean(eta_phi_cls[0,:])
     delta_etaCls = eta_phi_cls[0,:] - betaCls_mean
@@ -266,12 +239,6 @@ def prepare_fromSim(filename, suf, bins= [6,6,6], isPU=False):
     rel_eta_phi_z_cls = np.array([delta_etaCls, delta_phiCls, delta_zCls]).T
 
     #range_grid= [[-25,25], [-25,25],[-5,5]]
-    '''
-    nbins_x = 6 # Use a pair number
-    nbins_y = 6 # Use a pair number
-    nbins_z = 10 # Use a pair number
-    xy_bins_width = 5.
-    '''
     nbins_x = 30# Use a pair number
     nbins_y = 30 # Use a pair number
     nbins_z = 36 # Use a pair number
@@ -302,59 +269,23 @@ def prepare_fromSim(filename, suf, bins= [6,6,6], isPU=False):
     #print(f"bins_x: {bins_x}")
     #range_grid_cls= [[-25,25], [-25,25],[-25.,25.]]
     bins = (bins_x, bins_y, bins_z)
-    '''
-    # For eta-phi-z constant ranges
-    range_grid= [[-0.4,0.4], [-0.4,0.4],[-25.,25.]]
-    range_grid_cls= [[-0.4,0.4], [-0.4,0.4],[-25.,25.]]
-    range_grid = [[np.min(delta_etaTs), np.max(delta_etaTs)], 
-                  [np.min(delta_phiTs), np.max(delta_phiTs)],
-                  [np.min(delta_zTs),  np.max(delta_zTs)]]
-    # For eta-phi-z dinamic ranges
-    range_grid_cls = [[ np.min(delta_etaCls), np.max(delta_etaCls)],
-                  [np.min(delta_phiCls), np.max(delta_phiCls)],
-                  [np.min(delta_zCls), np.max(delta_zCls)]]
-    '''
 
-    #fTsM_g3D.append(np.array([np.histogramdd(pos_cls, bins=bins)[0], np.histogramdd(pos_cls, weights=np.asarray(en_ts), bins=bins)[0]]))
     pos_cls = pos_cls.T
     histo_pos = np.histogramdd(pos_cls, bins=bins)[0]
     histo_en = np.histogramdd(pos_cls, weights=np.asarray(en_cls), bins=bins)[0]
-    if np.sum(histo_pos, axis=None) ==0:
-      print("******* empty ********")
-      print(f"ev_info[i]: {ev_info[i]}")
-      print(f"bins_x: {bins_x}")
-      print(f"bins_y: {bins_y}")
-      print(f"bins_z: {bins_z}")
-      print(f"pos_cls: {pos_cls}")
-      #print(f"histo_pos: {histo_pos}")
-      #print(f"z_layers[bzCls_mean_tile_arg]: {z_layers[bzCls_mean_tile_arg]}")
-      #print(f"pos_cls[:,2]: {pos_cls[:,2]}")
-      print(f"bzCls_mean: {bzCls_mean}")
-      continue
-    fTsM_1D.append(np.array([bxTs_mean, byTs_mean, bzTs_mean]))
     fTsM_1D_cls.append(np.array([bxCls_mean, byCls_mean, bzCls_mean]))
     deltaTsM_1D_cls.append(rel_pos_cls)
     #deltaTsM_eta_phi_z_1D_cls.append(rel_eta_phi_z_cls)
 
-    #deltaTsM_1D.append(rel_pos)
-    #print(tTsM[i])
     tTsM_out.append(tTsM[i])
-    deltaTsM_1D.append(rel_pos)
-    deltaTsM_eta_phi_z_1D.append(rel_eta_phi_z)
     fTsM_g3D_cls.append(np.array([histo_pos,histo_en]))
 
+  print(f"none_reco: {none_reco}")
   tTsM_out = save_array_to_file(tTsM_out, f"{suf}_tTsM.parquet")
-  fTsM_1D = save_array_to_file(fTsM_1D, f"{suf}_fTsM_1D.parquet")
   fTsM_1D_cls = save_array_to_file(fTsM_1D_cls, f"{suf}_fTsM_1D_cls.parquet")
-  deltaTsM_1D = save_array_to_file(deltaTsM_1D, f"{suf}_deltaTsM_1D.parquet")
   deltaTsM_1D_cls = save_array_to_file(deltaTsM_1D_cls, f"{suf}_deltaTsM_1D_cls.parquet")
-  #fTsM_g3D = save_array_to_file(fTsM_g3D, f"{suf}_grid_3D.parquet")
   fTsM_g3D_cls = save_array_to_file(fTsM_g3D_cls, f"{suf}_grid_3D_cls.parquet")
-
-  print(f"fTsM_g3D_cls.type: {fTsM_g3D_cls.type}")
-  print(f"tTsM_out.type: {tTsM_out.type}")
-
-  return deltaTsM_1D, deltaTsM_1D_cls, fTsM_1D, fTsM_1D, fTsM_g3D, fTsM_g3D_cls, tTsM
+  return deltaTsM_1D_cls, fTsM_1D_cls, fTsM_g3D_cls, tTsM
 
 
 def main():
@@ -386,18 +317,12 @@ def main():
    #]
   bins = [12,12,12]
   for suf in file_sufix:
-    #deltaTsM_1D, fTsM_1D, fTsM_g3D, fTsM_pos, tTsM = prepare(f"data/histo_{suf}.root", suf, bins=bins, isPU=False)
-    deltaTsM_1D, deltaTsM_1D_cls, fTsM_1D, fTsM_1D_cls, fTsM_g3D, fTsM_g3D_cls, tTsM = prepare_fromSim(f"data/histo_{suf}.root", suf, bins=bins, isPU=False)
+    deltaTsM_1D_cls, fTsM_1D_cls, fTsM_g3D_cls, tTsM = prepare_fromSim(f"data/histo_{suf}.root", suf, bins=bins, isPU=False)
 
 
       
       
 
-  '''
-  # This solution takes up too much disk space.
-  with open('input.pkl', 'wb') as f:
-    pickle.dump(fTsM_g3D, f)
-  '''
 
 
 
